66870257aac59a43d5f80a1b6b2999712f57c73c
endroid <info@endroid.nl>
2016-04-08 22:45:30 +0000

    Add some checks

--- tests/QrCodeTest.php
+++ tests/QrCodeTest.php
@@ -9,25 +9,66 @@
 
 namespace Endroid\Tests\QrCode;
 
+use Endroid\QrCode\Exceptions\ImageFunctionFailedException;
+use Endroid\QrCode\Exceptions\ImageFunctionUnknownException;
 use Endroid\QrCode\QrCode;
 use PHPUnit_Framework_TestCase;
 
 class QrCodeTest extends PHPUnit_Framework_TestCase
 {
-    public function testCreateQrCode()
+    /**
+     * @var QrCode
+     */
+    protected $qrCode;
+
+    /**
+     * Tests if a valid data uri is returned.
+     */
+    public function testGetDataUri()
+    {
+        $qrCode = $this->getQrCode();
+        $dataUri = $qrCode->getDataUri();
+
+        $this->assertTrue(is_string($dataUri));
+    }
+
+    /**
+     * Tests if a valid image string is returned.
+     *
+     * @throws ImageFunctionFailedException
+     * @throws ImageFunctionUnknownException
+     */
+    public function testGetImageString()
+    {
+        $qrCode = $this->getQrCode();
+        $imageString = $qrCode->get('png');
+
+        $this->assertTrue(is_string($imageString));
+    }
+
+    /**
+     * Returns a QR code.
+     */
+    protected function getQrCode()
+    {
+        if (!$this->qrCode) {
+            $this->qrCode = $this->createQrCode();
+        }
+
+        return $this->qrCode;
+    }
+
+    /**
+     * Creates a QR code.
+     *
+     * @return QrCode
+     */
+    protected function createQrCode()
     {
         $qrCode = new QrCode();
         $qrCode->setText('Life is too short to be generating QR codes');
         $qrCode->setSize(300);
-        $qrCode->create();
 
-        $this->assertTrue(true);
-    }
-
-    public function testReadQrCode()
-    {
-        // TODO: check if the QR code is readable and contains the desired text
-
-        $this->assertTrue(true);
+        return $qrCode;
     }
 }
