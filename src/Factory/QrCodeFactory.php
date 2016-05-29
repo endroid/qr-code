08a4d29d46cef9e18420674a30afe07607bc6979
endroid <info@endroid.nl>
2016-05-29 20:41:55 +0000

    Make sure all available options are set

--- src/Factory/QrCodeFactory.php
+++ src/Factory/QrCodeFactory.php
@@ -26,6 +26,7 @@ class QrCodeFactory
      */
     public function __construct(array $defaults = array())
     {
+        $defaults = array_merge($this->getAvailableOptions(), $defaults);
         $this->optionsResolver = new OptionsResolver();
         $this->optionsResolver->setDefaults($defaults);
     }
@@ -43,46 +44,68 @@ class QrCodeFactory
 
         $qrCode = new QrCode();
 
-        if (isset($options['text'])) {
+        if (isset($options['text']) && !is_null($options['text'])) {
             $qrCode->setText($options['text']);
         }
 
-        if (isset($options['size'])) {
+        if (isset($options['size']) && !is_null($options['size'])) {
             $qrCode->setSize($options['size']);
         }
 
-        if (isset($options['padding'])) {
+        if (isset($options['padding']) && !is_null($options['padding'])) {
             $qrCode->setPadding($options['padding']);
         }
 
-        if (isset($options['extension'])) {
+        if (isset($options['extension']) && !is_null($options['extension'])) {
             $qrCode->setExtension($options['extension']);
         }
 
-        if (isset($options['error_correction_level'])) {
+        if (isset($options['error_correction_level']) && !is_null($options['error_correction_level'])) {
             $qrCode->setErrorCorrection($options['error_correction_level']);
         }
 
-        if (isset($options['foreground_color'])) {
+        if (isset($options['foreground_color']) && !is_null($options['foreground_color'])) {
             $qrCode->setForegroundColor($options['foreground_color']);
         }
 
-        if (isset($options['background_color'])) {
+        if (isset($options['background_color']) && !is_null($options['background_color'])) {
             $qrCode->setBackgroundColor($options['background_color']);
         }
 
-        if (isset($options['label'])) {
+        if (isset($options['label']) && !is_null($options['label'])) {
             $qrCode->setLabel($options['label']);
         }
 
-        if (isset($options['label_font_size'])) {
+        if (isset($options['label_font_size']) && !is_null($options['label_font_size'])) {
             $qrCode->setLabelFontSize($options['label_font_size']);
         }
 
-        if (isset($options['label_font_path'])) {
+        if (isset($options['label_font_path']) && !is_null($options['label_font_path'])) {
             $qrCode->setLabelFontPath($options['label_font_path']);
         }
 
         return $qrCode;
+    }
+
+    /**
+     * Returns all available options.
+     *
+     * @return array
+     */
+    public function getAvailableOptions()
+    {
+        $options = array(
+            'text' => null,
+            'size' => null,
+            'extension' => null,
+            'error_correction_level' => null,
+            'foreground_color' => null,
+            'background_color' => null,
+            'label' => null,
+            'label_font_size' => null,
+            'label_font_path' => null,
+        );
+
+        return $options;
     }
 }
