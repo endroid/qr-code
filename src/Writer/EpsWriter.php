2c5b527adf33440ad8f55aec11dafb3dd7816fb4
endroid <info@endroid.nl>
2017-04-29 20:54:13 +0000

    Add margin and fix whitespace issue in EPS

--- src/Writer/EpsWriter.php
+++ src/Writer/EpsWriter.php
@@ -34,6 +34,51 @@ class EpsWriter extends AbstractBaconWriter
             $this->convertErrorCorrectionLevel($this->qrCode->getErrorCorrectionLevel())
         );
 
+        $string = $this->addMargin($string);
+
+        return $string;
+    }
+
+    /**
+     * @param string $string
+     * @return string
+     */
+    protected function addMargin($string)
+    {
+        $targetSize = $this->qrCode->getSize() + $this->qrCode->getMargin() * 2;
+
+        $lines = explode("\n", $string);
+
+        $sourceBlockSize = 0;
+        $additionalWhitespace = $this->qrCode->getSize();
+        foreach ($lines as $line) {
+            if (preg_match('#[0-9]+ [0-9]+ [0-9]+ [0-9]+ F#i', $line) && strpos($line, $this->qrCode->getSize().' '.$this->qrCode->getSize().' F') === false) {
+                $parts = explode(' ', $line);
+                $sourceBlockSize = $parts[2];
+                $additionalWhitespace = min($additionalWhitespace, $parts[0]);
+            }
+        }
+
+        $blockCount = ($this->qrCode->getSize() - 2 * $additionalWhitespace) / $sourceBlockSize;
+        $targetBlockSize = $this->qrCode->getSize() / $blockCount;
+
+        foreach ($lines as &$line) {
+            if (strpos($line, 'BoundingBox') !== false) {
+                $line = '%%BoundingBox: 0 0 '.$targetSize.' '.$targetSize;
+            } elseif (strpos($line, $this->qrCode->getSize().' '.$this->qrCode->getSize().' F') !== false) {
+                $line = '0 0 '.$targetSize.' '.$targetSize.' F';
+            } elseif (preg_match('#[0-9]+ [0-9]+ [0-9]+ [0-9]+ F#i', $line)) {
+                $parts = explode(' ', $line);
+                $parts[0] = $this->qrCode->getMargin() + $targetBlockSize * ($parts[0] - $additionalWhitespace) / $sourceBlockSize;
+                $parts[1] = $this->qrCode->getMargin() + $targetBlockSize * ($parts[1] - $sourceBlockSize - $additionalWhitespace) / $sourceBlockSize;
+                $parts[2] = $targetBlockSize;
+                $parts[3] = $targetBlockSize;
+                $line = implode(' ', $parts);
+            }
+        }
+
+        $string = implode("\n", $lines);
+
         return $string;
     }
 
