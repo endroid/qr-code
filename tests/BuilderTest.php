00679ba7b39d5ad1dbed890b5c23f577fe5f0cc1
Daniele Pastori <fullbl@mail.com>
2021-04-26 11:09:03 +0200

    tests for svg logo

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -12,17 +12,21 @@ use Endroid\QrCode\Label\Font\NotoSans;
 use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
 use Endroid\QrCode\Writer\PngWriter;
 use Endroid\QrCode\Writer\Result\PngResult;
+use Endroid\QrCode\Writer\Result\SvgResult;
+use Endroid\QrCode\Writer\SvgWriter;
+use Endroid\QrCode\Writer\WriterInterface;
 use PHPUnit\Framework\TestCase;
 
 final class BuilderTest extends TestCase
 {
     /**
      * @testdox Write advanced example via builder
+     * @dataProvider imagesDataProvider
      */
-    public function testBuilder(): void
+    public function testBuilder(WriterInterface $writer, string $logoPath, string $resultClass, string $mimeType): void
     {
         $result = Builder::create()
-            ->writer(new PngWriter())
+            ->writer($writer)
             ->writerOptions([])
             ->data('Custom QR code contents')
             ->encoding(new Encoding('UTF-8'))
@@ -30,14 +34,44 @@ final class BuilderTest extends TestCase
             ->size(300)
             ->margin(10)
             ->roundBlockSizeMode(new RoundBlockSizeModeMargin())
-            ->logoPath(__DIR__.'/assets/symfony.png')
+            ->logoPath($logoPath)
             ->labelText('This is the label')
             ->labelFont(new NotoSans(20))
             ->labelAlignment(new LabelAlignmentCenter())
             ->build()
         ;
 
-        $this->assertInstanceOf(PngResult::class, $result);
-        $this->assertEquals('image/png', $result->getMimeType());
+        $this->assertInstanceOf($resultClass, $result);
+        $this->assertEquals($mimeType, $result->getMimeType());
+    }
+
+    public function imagesDataProvider(): array
+    {
+        return [
+            'png qr, png logo' => [
+                new PngWriter(),
+                __DIR__.'/assets/symfony.png',
+                PngResult::class,
+                'image/png',
+            ],
+            'png qr, svg logo' => [
+                new PngWriter(),
+                __DIR__.'/assets/symfony.svg',
+                PngResult::class,
+                'image/png',
+            ],
+            'svg qr, svg logo' => [
+                new SvgWriter(),
+                __DIR__.'/assets/symfony.svg',
+                SvgResult::class,
+                'image/svg+xml',
+            ],
+            'svg qr, png logo' => [
+                new SvgWriter(),
+                __DIR__.'/assets/symfony.png',
+                SvgResult::class,
+                'image/svg+xml',
+            ]
+        ];
     }
 }
