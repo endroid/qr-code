9c507ea522454f0d5cb29be0911fe275463b8999
Jeroen van den Enden <info@endroid.nl>
2022-01-31 17:21:56 +0000

    Add another test for Github issue

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Endroid\QrCode\Tests;
 
 use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Color\Color;
 use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
 use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
 use Endroid\QrCode\Label\Font\NotoSans;
+use Endroid\QrCode\QrCode;
 use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
 use Endroid\QrCode\Writer\PngWriter;
 use Endroid\QrCode\Writer\Result\PngResult;
@@ -61,6 +63,21 @@ final class BuilderTest extends TestCase
             ->labelFont(new NotoSans(20))
             ->labelAlignment(new LabelAlignmentCenter())
             ->build();
+
+        $this->assertInstanceOf(PngResult::class, $result);
+        $this->assertEquals('image/png', $result->getMimeType());
+
+        $writer = new PngWriter();
+        $qrCode = QrCode::create('https://xxxxxxx.xxxx/xxxxxxx')
+            ->setEncoding(new Encoding('UTF-8'))
+            ->setErrorCorrectionLevel(new ErrorCorrectionLevelHigh())
+            ->setSize(1200)
+            ->setMargin(40)
+            ->setRoundBlockSizeMode(new RoundBlockSizeModeMargin())
+            ->setForegroundColor(new Color(0, 0, 0))
+            ->setBackgroundColor(new Color(255, 255, 255, 127));
+
+        $result = $writer->write($qrCode);
 
         $this->assertInstanceOf(PngResult::class, $result);
         $this->assertEquals('image/png', $result->getMimeType());
