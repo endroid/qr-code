7a16f5129150340c0f5937b224f69bf66c7be4bf
Tobias Feijten <tobias93@live.nl>
2024-10-29 20:25:44 +0100

    Add optional arguments to interface, to enhance compatibility (#456)

--- src/Builder/BuilderInterface.php
+++ src/Builder/BuilderInterface.php
@@ -4,9 +4,41 @@ declare(strict_types=1);
 
 namespace Endroid\QrCode\Builder;
 
+use Endroid\QrCode\Color\ColorInterface;
+use Endroid\QrCode\Encoding\EncodingInterface;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\Label\Font\FontInterface;
+use Endroid\QrCode\Label\LabelAlignment;
+use Endroid\QrCode\Label\Margin\MarginInterface;
+use Endroid\QrCode\RoundBlockSizeMode;
 use Endroid\QrCode\Writer\Result\ResultInterface;
+use Endroid\QrCode\Writer\WriterInterface;
 
 interface BuilderInterface
 {
-    public function build(): ResultInterface;
+    public function build(
+        ?WriterInterface $writer = null,
+        ?array $writerOptions = null,
+        ?bool $validateResult = null,
+        // QrCode options
+        ?string $data = null,
+        ?EncodingInterface $encoding = null,
+        ?ErrorCorrectionLevel $errorCorrectionLevel = null,
+        ?int $size = null,
+        ?int $margin = null,
+        ?RoundBlockSizeMode $roundBlockSizeMode = null,
+        ?ColorInterface $foregroundColor = null,
+        ?ColorInterface $backgroundColor = null,
+        // Label options
+        ?string $labelText = null,
+        ?FontInterface $labelFont = null,
+        ?LabelAlignment $labelAlignment = null,
+        ?MarginInterface $labelMargin = null,
+        ?ColorInterface $labelTextColor = null,
+        // Logo options
+        ?string $logoPath = null,
+        ?int $logoResizeToWidth = null,
+        ?int $logoResizeToHeight = null,
+        ?bool $logoPunchoutBackground = null,
+    ): ResultInterface;
 }
